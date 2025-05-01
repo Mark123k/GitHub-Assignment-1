75378649f766f11705066c61216f2a1f2e6907d5
Mark123k <markkratos5252@gmail.com>
2025-05-02 00:21:07 +0530

    add changes in branch b2

--- hello.sh
+++ hello.sh
@@ -2,3 +2,5 @@
 echo "Hello World! Im learning Devops"
 
 echo "This is the scripting language"
+
+echo "This is a bash script modified in b2"
--- test.py
+++ test.py
@@ -1,1 +1,4 @@
 print("This is a Python script")
+print("This is a Python script")
+x = 10
+print(f"Value added in b2: {x}")
