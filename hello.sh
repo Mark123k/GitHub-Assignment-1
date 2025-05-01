a9130ac7154e9e7fbb8eef06704cf257dd82a535
Mark123k <markkratos5252@gmail.com>
2025-05-02 00:18:53 +0530

    add changes in branch b1

--- hello.sh
+++ hello.sh
@@ -2,3 +2,5 @@
 echo "Hello World! Im learning Devops"
 
 echo "This is the scripting language"
+
+echo "new line added to the b1"
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
 print("This is a Python script")
+
+a = 3
+b = 3
+c= a + b
+print("the sum is = ", c)
